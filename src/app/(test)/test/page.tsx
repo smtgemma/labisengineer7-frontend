5bcc1fe488bc2d77392417d3352b37bbe8c63d1a
shahidul islam <shahidulislamshohid7@gmail.com>
2025-08-18 09:30:48 +0600

    template show is done

--- src/app/(test)/test/page.tsx
+++ src/app/(test)/test/page.tsx
@@ -1,15 +1,4 @@
 "use client"
-
-import FileOneDesignEight from "@/components/CreateProject/FinalOverview/file-one/design-eight/page"
-import FileOneDesignEleven from "@/components/CreateProject/FinalOverview/file-one/design-eleven/page"
-import FileOneDesignFive from "@/components/CreateProject/FinalOverview/file-one/design-five/page"
-import FileOneDesignFour from "@/components/CreateProject/FinalOverview/file-one/design-four/page"
-import FileOneDesignThirteen from "@/components/CreateProject/FinalOverview/file-one/design-thirteen/page"
-import FileOneDesignTweleve from "@/components/CreateProject/FinalOverview/file-one/design-tweleve/page"
-import FileOneDesignSeven from "@/components/CreateProject/FinalOverview/file-one/design-seven/page"
-import FileOneDesignSix from "@/components/CreateProject/FinalOverview/file-one/design-six/page"
-import FileOneDesignNine from "@/components/CreateProject/FinalOverview/file-one/design-nine/page"
-import FileOneDesignTen from "@/components/CreateProject/FinalOverview/file-one/design-ten/page"
 import F1D1 from "@/components/CreateProject/FinalOverview/f-01/f1D1/page"
 import F1D2 from "@/components/CreateProject/FinalOverview/f-01/f1D2/page"
 import F1D3 from "@/components/CreateProject/FinalOverview/f-01/f1D3/page"
@@ -19,31 +8,33 @@ import F1D6 from "@/components/CreateProject/FinalOverview/f-01/f1D6/page"
 import F1D7 from "@/components/CreateProject/FinalOverview/f-01/f1D7/page"
 import F1D8 from "@/components/CreateProject/FinalOverview/f-01/f1D8/page"
 import F1D9 from "@/components/CreateProject/FinalOverview/f-01/f1D9/page"
+import F2D1 from "@/components/CreateProject/FinalOverview/f-02/f2D1/page"
+import F2D2 from "@/components/CreateProject/FinalOverview/f-02/f2D2/page"
+import F2D3 from "@/components/CreateProject/FinalOverview/f-02/f2D3/page"
+import F3D1 from "@/components/CreateProject/FinalOverview/f-03/f3D1/page"
+import F3D2 from "@/components/CreateProject/FinalOverview/f-03/f3D2/page"
 function test() {
   return (
     <div>
-      {/* <FileOneDesignThree/> */}
-      {/* <FileOneDesignFour/>
-      <FileOneDesignFive/>
-      <FileOneDesignSix/>
-      <FileOneDesignSeven/>
-      <FileOneDesignEight/>
-      <FileOneDesignNine/>
-      <FileOneDesignTen/>
-      <FileOneDesignEleven/>
-      <FileOneDesignTweleve />
-      <FileOneDesignThirteen/> */}
-
       {/* file one  */}
-      <F1D1/>
-      <F1D2/>
-      <F1D3/>
-      <F1D4/>
-      <F1D5/>
-      <F1D6/>
-      <F1D7/>
-      <F1D8/>
-      <F1D9/>
+      {/* <F1D1/> */}
+      {/* <F1D2/> */}
+      {/* <F1D3/> */}
+      {/* <F1D4/> */}
+      {/* <F1D5/> */}
+      {/* <F1D6/> */}
+      {/* <F1D7/> */}
+      {/* <F1D8/> */}
+      {/* <F1D9/> */}
+
+      {/* file two  */}
+      {/* <F2D1/> */}
+      {/* <F2D2/> */}
+      {/* <F2D3/> */}
+      
+      {/* file three  */}
+      {/* <F3D1/> */}
+      <F3D2/>
     </div>
   )
 }
