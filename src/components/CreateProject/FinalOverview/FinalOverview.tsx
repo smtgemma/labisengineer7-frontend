5b47cd67ea91da65d247718ee22388c6e58087ca
shahidul islam <shahidulislamshohid7@gmail.com>
2025-09-22 20:38:14 +0600

    template f7d7 is done

--- src/components/CreateProject/FinalOverview/FinalOverview.tsx
+++ src/components/CreateProject/FinalOverview/FinalOverview.tsx
@@ -64,6 +64,7 @@ import F7D3 from "./f-07/f7D3/page";
 import F7D4 from "./f-07/f7D4/page";
 import F7D5 from "./f-07/f7D5/page";
 import F7D6 from "./f-07/f7D6/page";
+import F7D7 from "./f-07/f7D6/f7D7/page";
 
 export interface UserData {
   id: string;
@@ -640,7 +641,7 @@ const FinalOverview: React.FC<FinalOverviewProps> = ({
                 <F7D6 allData={allData} /> // 6
               )}
               {selected === "seven_seven" && (
-                <F10D15 allData={allData} /> // 7
+                <F7D7 allData={allData} /> // 7
               )}
               {selected === "seven_eight" && (
                 <F10D15 allData={allData} /> // 8
