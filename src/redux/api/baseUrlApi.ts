65946c889fdbcefea36af9966b0efa1bead0354a
shahidul islam <shahidulislamshohid7@gmail.com>
2025-10-21 09:16:38 +0600

    validation error fix

--- src/redux/api/baseUrlApi.ts
+++ src/redux/api/baseUrlApi.ts
@@ -5,7 +5,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseUrlApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://api.buildai.gr/api/v1",
+    // baseUrl: "https://api.buildai.gr/api/v1",
+    baseUrl: "https://mc999nnm-8145.inc1.devtunnels.ms/api/v1",
     // baseUrl: "https://api.buildai.gr/api/v1",
     // prepareHeaders: (headers) => {
     //   const token = Cookies.get("accessToken");
